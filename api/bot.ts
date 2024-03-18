6604b31987ba5d2b597c68e9d19d12ee263cd7e5
jianghong <jianghong2020@qq.com>
2024-03-18 10:36:59 +0800

    vercel

---
+++ api/bot.ts
@@ -0,0 +1,14 @@
+import { Bot, webhookCallback } from "grammy";
+import { initCallback, initCommand } from "./commands/config";
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is unset");
+
+const bot = new Bot(token);
+initCommand(bot)
+bot.catch((err) => {
+    console.log(err.message);
+})
+bot.callbackQuery(/.+/gi, async (ctx) => {
+    initCallback(ctx)
+})
+export default webhookCallback(bot, "http");
