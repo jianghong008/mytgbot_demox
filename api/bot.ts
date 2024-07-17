ad4d8317c6a7752bce951a8c880b1b17aea86dbb
jianghong <jianghong2020@qq.com>
2024-07-17 15:01:24 +0800

    测试

--- api/bot.ts
+++ api/bot.ts
@@ -2,7 +2,7 @@ import { Bot, webhookCallback } from "grammy";
 import { initCallback, initCommand } from "./commands/config";
 const token = process.env.BOT_TOKEN;
 if (!token) throw new Error("BOT_TOKEN is unset");
-
+console.log(token)
 const bot = new Bot(token);
 initCommand(bot)
 bot.catch((err) => {
